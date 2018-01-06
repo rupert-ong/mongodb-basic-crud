3344b9054cc7b59c41505f0f51a5ee371e0942de
ongr <Rupert.Ong@pointclickcare.com>
2018-01-06 09:24:25 -0500

    Add model

---
+++ models/MarioChar.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema();
+
+// Create schema and model
+const MarioCharSchema = new Schema({
+  name: String,
+  weight: Number
+});
+
+const MarioChar = mongoose.model('MarioChar', MarioCharSchema);
+
+module.exports = MarioChar;

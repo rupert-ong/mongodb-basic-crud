a34c91fb99e29f46dd1bb0993b38e8bde37b6c2d
rupert-ong <rupert.ong@gmail.com>
2018-01-07 15:39:46 -0500

    Add ES6 Promise. Implement before method hook for mocha

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/testaroo');
+// ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once('open', () => {
-  console.log('Connection has been made');
-}).on('error', err => {
-  console.log(`Connection error: ${err}`);
+// Connect to DB before tests run
+before((done) => {
+  // Connect to MongoDB
+  mongoose.connect('mongodb://localhost/testaroo');
+  
+  mongoose.connection.once('open', () => {
+    console.log('Connection has been made');
+    done();
+  }).on('error', err => {
+    console.log(`Connection error: ${err}`);
+  });
 });

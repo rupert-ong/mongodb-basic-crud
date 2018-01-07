eadc80f63a6a2c95392c52351ed816348f7a1cbb
ongr <Rupert.Ong@pointclickcare.com>
2018-01-07 14:21:47 -0500

    Add mocha and demo test

---
+++ test/demo_test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+// Describe tests
+describe('some demo tests', () => {
+  // Create tests
+  it('adds 2 number together', () => {
+    assert(2 + 3 === 5, )
+  });
+});

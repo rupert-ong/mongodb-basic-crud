d150d49e851a1808f21152e7c09c5adfc38c234a
rupert-ong <rupert.ong@gmail.com>
2018-01-08 12:49:02 -0500

    Add find by ID test

--- test/finding_test.js
+++ test/finding_test.js
@@ -1,19 +1,29 @@
 const assert = require('assert');
 const MarioChar = require('../models/MarioChar');
 
+// Make char available in scopes
+var char;
+
 describe('Finding records', () => {
   beforeEach((done)=> {
-    var char = new MarioChar({ name: 'Mario' });
+    char = new MarioChar({ name: 'Mario' });
     char.save().then(()=> {
       done();
     })
   });
 
-  // Single test
-  it('Find a single record in the database', (done) => {
+  // Tests
+  it('Finds one record from the database', (done) => {
     MarioChar.findOne({ name: 'Mario' }).then((result) => {
       assert(result.name === 'Mario');
       done();
     });
+  });
+
+  it('Finds one record by ID from the database', (done) => {
+    MarioChar.findOne({ _id: char._id }).then((result) => {
+      assert(result._id.toString() === char._id.toString());
+      done();
+    })
   });
 });
